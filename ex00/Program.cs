b01a5356bcafd0589e32836a82d7ff1af613fddd
Your Name <you@example.com>
2021-05-24 16:52:31 +0300

    finish

---
+++ ex00/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+/*
+sum  double  Сумма кредита, р 1000000
+rate  double Годовая процентная ставка, % 12
+term  int Количество месяцев кредита 10 
+selectedMonth int Номер месяца кредита, в котором вносится досрочный платёж 5
+payment double Сумма досрочного платежа, р 100000 */
+
+double sum;
+double rate;
+int    term;
+int    selectedMonth;
+double payment;
+
+double i; // процентная ставка по займу в месяц
+double anum; // анументный платеж
+double proc = 0; // проценты ежемесячного платежа
+double proc_minussum = 0; // переплата за уменьшение суммы платежа
+double proc_minsdate = 0; // переплата за уменьшение месяцев
+int month = 0; // месяц в период
+int year; // текущий год
+int year_day; // дней в году
+
+if (args.Length != 5)
+{
+    Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+    return;
+}
+if (!double.TryParse(args[0], out sum) || !double.TryParse(args[1], out rate) ||
+    !int.TryParse(args[2], out term) || !int.TryParse(args[3], out selectedMonth) ||
+    !double.TryParse(args[4], out payment))
+{
+    Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+    return;  
+}
+i = rate / 12 / 100;
+anum = Math.Round(sum * i * Math.Pow(1 + i, term) / ((Math.Pow(1 + i, term) - 1)), 2);
+year = DateTime.Now.Year;
+Console.WriteLine("ПЛАТЕЖ         ОД       ПРОЦЕНТЫ    ОСТАТОК ДОЛГА");
+
+MinusSum(term, sum, payment, selectedMonth, rate, year, month, proc, anum);
+Console.WriteLine("ПЛАТЕЖ         ОД       ПРОЦЕНТЫ    ОСТАТОК ДОЛГА");
+MinusDate(term, sum, payment, selectedMonth, rate, year, month, proc);
+WriteMess();
+
+
+void MinusSum(int term, double sum, double payment, int selectedMonth, double rate, int year, int month, double proc, double anum)
+{
+    for (int m = 1; m <= term; m++)
+    {
+        month = m - 1 + DateTime.Now.Month;
+        while (month > 12)
+            month -= 12;
+        if (m != 1 && month == 1)
+            year++;
+        if (DateTime.IsLeapYear(year))
+            year_day = 366;
+        else
+            year_day = 365;
+        proc = Math.Round(sum * rate * DateTime.DaysInMonth(year, month) / (100 * year_day), 2);
+        sum -= anum - proc;
+        sum = Math.Round(sum, 2);
+        proc_minussum += Math.Round(proc, 2);
+        if (m == selectedMonth)
+        {
+            sum -= payment;
+            anum = Math.Round(sum * i * Math.Pow(1 + i, term - m) / ((Math.Pow(1 + i, term - m) - 1)), 2);
+        }
+        Console.WriteLine(anum + "    " + Math.Round((anum - proc), 2) + "    " + proc + "    " + sum);
+    }
+    Console.WriteLine("\tОбщая переплата " + Math.Round(proc_minussum, 2)); 
+}
+
+
+void MinusDate(int term, double sum, double payment, int selectedMonth, double rate, int year, int month, double proc)
+{
+    for (int m = 1; m <= term; m++)
+    {
+        month = m - 1 + DateTime.Now.Month;
+        while (month > 12)
+            month -= 12;
+        if (m != 1 && month == 1)
+            year++;
+        if (DateTime.IsLeapYear(year))
+            year_day = 366;
+        else
+            year_day = 365;
+        proc = Math.Round(sum * rate * DateTime.DaysInMonth(year, month) / (100 * year_day), 2);
+        if (sum + proc < anum)
+            break;
+        sum -= anum - proc;
+        sum = Math.Round(sum, 2);
+        proc_minsdate += Math.Round(proc, 2);
+        if (m == selectedMonth)
+            sum -= payment;
+        Console.WriteLine(anum + "    " + Math.Round((anum - proc), 2) + "    " + proc + "    " + sum);
+    }
+    proc_minsdate += proc;
+    Console.WriteLine("\tОбщая переплата " + Math.Round(proc_minsdate, 2)); 
+}
+
+void WriteMess()
+{
+    if (proc_minussum > proc_minsdate)
+    {
+        Console.WriteLine("Переплата при уменьшении платежа: " + Math.Round(proc_minussum, 2) + "p.");
+        Console.WriteLine("Переплата при уменьшении срока: " + Math.Round(proc_minsdate, 2) + "p.");
+        Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа на: " + Math.Round(proc_minussum - proc_minsdate, 2) + "p.");
+    }
+    else if (proc_minussum < proc_minsdate)
+    {
+        Console.WriteLine("Переплата при уменьшении платежа: " + Math.Round(proc_minussum, 2) + "p.");
+        Console.WriteLine("Переплата при уменьшении срока: " + Math.Round(proc_minsdate, 2) + "p.");
+        Console.WriteLine("Уменьшение платежа выгоднее уменьшения срока на: " + Math.Round(proc_minussum - proc_minsdate, 2) + "p.");     
+    }
+    else
+    {
+        Console.WriteLine("Переплата при уменьшении платежа: " + Math.Round(proc_minussum, 2) + "p.");
+        Console.WriteLine("Переплата при уменьшении срока: " + Math.Round(proc_minsdate, 2) + "p.");  
+    }
+}
---
+++ ex01/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+
+Console.Write(">Enter name: ");
+var name = Console.ReadLine();
+string path; // file name
+string[] str; // string array from file
+string[] str_name2;
+string[] str_name3;
+int dist;
+int a = 0, s = 0;
+
+if (name.Length == 0)
+{
+    Console.WriteLine("Your name was not found.");
+    return;   
+}
+if (CheckName(name) == 1)
+{
+    Console.WriteLine("Error name");
+    return;
+}
+path = Path.GetFullPath("names.txt");
+if (!File.Exists(path))
+{
+    Console.WriteLine("Error file");
+    return;
+}
+str = File.ReadAllLines(path);
+str_name2 = new string[str.Length];
+str_name3 = new string[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    if (name == str[i])
+    {
+        Console.WriteLine("Hello, " + str[i] + "!");
+        return;
+    }
+}
+for (int i = 0; i < str.Length; i++)
+{
+    dist = DamerauLevenshteinDistance(name, str[i]);
+    if (dist == 1)
+    {
+        Console.Write(">Did you mean " + str[i] + "? Y/N ");
+        if (Console.ReadKey().KeyChar == 'y')
+        {
+            Console.WriteLine("\nHello, " + str[i] + "!");
+            return;  
+        }
+        Console.WriteLine();
+    }
+
+    if (dist == 2)
+        str_name2[a++] = str[i];
+    if (dist == 3)
+        str_name3[s++] = str[i];
+}
+for (int i = 0; i < str_name2.Length; i++)
+{
+    if (str_name2[i] == null)
+        break;
+    Console.Write(">2Did you mean " + str_name2[i] + "? Y/N ");
+    if (Console.ReadKey().KeyChar == 'y')
+    {
+        Console.WriteLine("\nHello, " + str_name2[i] + "!");
+        return;
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < str_name3.Length; i++)
+{
+    if (str_name3[i] == null)
+        break;
+    Console.Write(">3Did you mean " + str_name3[i] + "? Y/N ");
+    if (Console.ReadKey().KeyChar == 'y')
+    {
+        Console.WriteLine("\nHello, " + str_name3[i] + "!");
+        return;
+    }
+    Console.WriteLine();   
+}
+Console.WriteLine("Your name was not found.");
+
+static int MinimumDwo(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+static int Minimum(int a, int b, int c)
+{
+    return (a = a < b ? a : b) < c ? a : c;
+}
+
+static int DamerauLevenshteinDistance(string nameTmp, string strTmp)
+{
+    var n = nameTmp.Length + 1;
+    var m = strTmp.Length + 1;
+    var arrayD = new int[n, m];
+
+    for (var i = 0; i < n; i++)
+    {
+        arrayD[i, 0] = i;
+    }
+    for (var j = 0; j < m; j++)
+    {
+        arrayD[0, j] = j;
+    }
+    for (var i = 1; i < n; i++)
+    {
+        for (var j = 1; j < m; j++)
+        {
+            var cost = nameTmp[i - 1] == strTmp[j - 1] ? 0 : 1;
+
+            arrayD[i, j] = Minimum(arrayD[i - 1, j] + 1,          // удаление
+                arrayD[i, j - 1] + 1,         // вставка
+                arrayD[i - 1, j - 1] + cost); // замена
+
+            if (i > 1 && j > 1 
+                      && nameTmp[i - 1] == strTmp[j - 2]
+                      && nameTmp[i - 2] == strTmp[j - 1])
+            {
+                arrayD[i, j] = MinimumDwo(arrayD[i, j],
+                    arrayD[i - 2, j - 2] + cost); // перестановка
+            }
+        }
+    }
+    return arrayD[n - 1, m - 1];
+}
+
+static int CheckName(string nameTmp)
+{
+    var spec = "!@#$%^&*()+-={}[];:*\\\"\'";
+    
+    for (int i = 0; i < nameTmp.Length; i++)
+    {
+        if (Char.IsDigit(nameTmp[i]) || spec.IndexOf(nameTmp[i]) != -1)
+            return 1;
+    }
+    return 0;
+}
